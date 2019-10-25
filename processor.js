082c4fe25f4594ddab37d3e1747341aebb730add
hm <hide-mikami@omni.co.jp>
2019-10-25 15:12:13 +0900

    test1025

--- processor.js
+++ processor.js
@@ -13,6 +13,7 @@ let processor = {
         this.video = document.getElementById("video");
         this.img = document.getElementById("img");
         //this.button = document.getElementById("button");
+        //this.button = document.getElementById("button");
         //this.button.addEventListener('click', function () {
             
         //}, false);
@@ -29,7 +30,7 @@ let processor = {
   
     computeFrame: function () {
         //this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-        this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+        this.ctx1.drawImage(this.video, 0, 0, window.innerWidth, window.innerHeight);
         this.ctx1.drawImage(this.img, 0, 0, window.innerWidth, window.innerHeight - 18);
         //this.ctx1.drawImage(this.button, this.width / 4, this.height/2-100, 50, 50);
         return;
